27c2c391d09608930e76245585bb277ab0a8abde
nosnowowie93347 <65568190+nosnowowie93347@users.noreply.github.com>
2020-08-09 07:14:32 -0700

    Add files via upload

--- Items/GodlyHealingPotion.cs
+++ Items/GodlyHealingPotion.cs
@@ -28,6 +28,13 @@ namespace MyFirstBasicMod.Items
 			item.healLife = 100; // While we change the actual healing value in GetHealLife, item.healLife still needs to be higher than 0 for the item to be considered a healing item
 			item.potion = true; // Makes it so this item applies potion sickness on use and allows it to be used with quick heal
 		}
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<Items.Placeable.PinksBar>(), 9);
+            recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this, 2);
+			recipe.AddRecipe();
+		}
 
 		public override void GetHealLife(Player player, bool quickHeal, ref int healValue) {
 			// Make the item heal half the player's max health normally, or one fourth if used with quick heal
