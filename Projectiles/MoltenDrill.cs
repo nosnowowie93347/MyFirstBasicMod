cc441405b0cde9937a83bfd44804b397531f5ddc
nosnowowie93347 <nosnowowie@Gmail.com>
2020-10-06 15:31:30 -0700

    Add files via upload

---
+++ Projectiles/MoltenDrill.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MyFirstBasicMod.Projectiles
+{
+	//ported from my tAPI mod because I don't want to make artwork
+	public class MoltenDrill : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.width = 22;
+			projectile.height = 22;
+			projectile.aiStyle = 20;
+			projectile.friendly = true;
+			projectile.penetrate = -1;
+			projectile.tileCollide = false;
+			projectile.hide = true;
+			projectile.ownerHitCheck = true; //so you can't hit enemies through walls
+			projectile.melee = true;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			if (Main.rand.NextBool(10))
+			{
+				target.AddBuff(BuffID.OnFire, 180, false);
+			}
+		}
+
+		public override void OnHitPvp(Player target, int damage, bool crit)
+		{
+			if (Main.rand.NextBool(10))
+			{
+				target.AddBuff(BuffID.OnFire, 180, false);
+			}
+		}
+	}
+}
---
+++ Projectiles/PixelBall.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace MyFirstBasicMod.Projectiles
+{
+	public class PixelBall : ElementBall
+	{
+		public override string Texture => mod.Name + "/Projectiles/ElementBall";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Pixel Ball");
+		}
+
+		public override void PlaySound()
+		{
+			Main.PlaySound(SoundID.Item33, projectile.position);
+		}
+
+		public override string GetName()
+		{
+			if (projectile.ai[0] == 24f)
+			{
+				return "Fire Sprite";
+			}
+			if (projectile.ai[0] == 44f)
+			{
+				return "Frost Sprite";
+			}
+			if (projectile.ai[0] == 70f)
+			{
+				return "Infestation Sprite";
+			}
+			if (projectile.ai[0] == 69f)
+			{
+				return "Ichor Sprite";
+			}
+			return "Doom Bubble";
+		}
+	}
+}
