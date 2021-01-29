b75b753b86ae3948b1ddcc4a3e2f0b61252fc511
nosnowowie93347 <nosnowowie@Gmail.com>
2021-01-29 10:51:39 -0800

    updating code, adding a few things

---
+++ Items/Accessories/EpicShield.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MyFirstBasicMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Shield)]
+    public class EpicShield : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Epic Shield");
+            Tooltip.SetDefault("Provides immunity to a few important debuffs\nAs health decreases, defense increases");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 36;
+            item.rare = ItemRarityID.Pink;
+            item.value = 100000;
+            item.accessory = true;
+            item.defense = 6;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.PocketMirror, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.noKnockback = true;
+            float defBoost = (float)(player.statLifeMax2 - player.statLife) / (float)player.statLifeMax2 * 20f;
+            player.statDefense += (int)defBoost;
+            player.buffImmune[BuffID.Stoned] = true;
+            player.buffImmune[BuffID.Obstructed] = true;
+            player.buffImmune[BuffID.OnFire] = true;
+            player.buffImmune[BuffID.Poisoned] = true;
+            player.buffImmune[BuffID.Frozen] = true;
+        }
+    }
+}
---
+++ Items/Accessories/GreenRing.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MyFirstBasicMod.Items.Accessories
+{
+    public class GreenRing : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Emerald Ring");
+            Tooltip.SetDefault("5% increased damage");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 40;
+            item.height = 40;
+            item.value = Item.sellPrice(0, 0, 70, 0);
+            item.rare = ItemRarityID.Green;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.meleeDamage += .05f;
+            player.magicDamage += .05f;
+            player.thrownDamage += .05f;
+            player.rangedDamage += .05f;
+            player.minionDamage += .05f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.GoldBar, 4);
+            recipe.AddIngredient(ItemID.Emerald, 3);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
