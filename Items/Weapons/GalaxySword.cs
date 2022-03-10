2d1bb28ddabaf974f5e45e55afcfcb0992a0b5a5
nosnowowie93347 <nosnowowie@Gmail.com>
2022-03-09 16:13:33 -0800

    Finally, an update!!

---
+++ Items/Weapons/GalaxySword.cs
@@ -0,0 +1,42 @@
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+namespace MyFirstBasicMod.Items.Weapons
+{
+	public class Test : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Galaxy Sword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Fancy");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 115;
+			item.melee = true;
+			item.width = 30;
+			item.height = 60;
+			item.useTime = 14;
+			item.useAnimation = 14;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 5.5f;
+			item.value = 99999;
+			item.rare = ItemRarityID.LightPurple;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Items.Placeable.PinksBar>(), 30);
+            recipe.AddIngredient(ItemID.Starfury, 1);
+            recipe.AddIngredient(ItemID.HellstoneBar, 10);
+			recipe.AddTile(TileType<Tiles.PinksAnvil>());
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
