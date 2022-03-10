0dbe7f262ce0427598a489e1f08761ec04f835f9
nosnowowie93347 <nosnowowie@Gmail.com>
2022-03-09 16:38:57 -0800

    Update TwilightsBow.cs

--- Items/TwilightsBow.cs
+++ Items/TwilightsBow.cs
@@ -18,7 +18,7 @@ namespace MyFirstBasicMod.Items
 		public override void SetDefaults() {
 			item.damage = 107;
 			item.ranged = true;
-            item.crit = 14;
+            		item.crit = 14;
 			item.width = 34;
 			item.height = 55;
 			item.useTime = 12;
@@ -44,15 +44,6 @@ namespace MyFirstBasicMod.Items
 			recipe.AddRecipe();
 		}
 
-		/*
-		 * Feel free to uncomment any of the examples below to see what they do
-		 */
-
-		// What if I wanted this gun to have a 38% chance not to consume ammo?
-		
-
-        // What if I wanted it to work like Uzi, replacing regular bullets with High Velocity Bullets?
-        // Uzi/Molten Fury style: Replace normal Bullets with Highvelocity
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
             if (type == ProjectileID.VenomArrow) // or ProjectileID.WoodenArrowFriendly
@@ -60,83 +51,5 @@ namespace MyFirstBasicMod.Items
                 type = ProjectileType<Projectiles.TwilightArrow>(); // or ProjectileID.FireArrow;
             }
             return true; // return true to allow tmodloader to call Projectile.NewProjectile as normal
-        }
-
-        // What if I wanted it to shoot like a shotgun?
-        // Shotgun style: Multiple Projectiles, Random spread 
-        /*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			int numberProjectiles = 4 + Main.rand.Next(2); // 4 or 5 shots
-			for (int i = 0; i < numberProjectiles; i++)
-			{
-				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(30)); // 30 degree spread.
-				// If you want to randomize the speed to stagger the projectiles
-				// float scale = 1f - (Main.rand.NextFloat() * .3f);
-				// perturbedSpeed = perturbedSpeed * scale; 
-				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
-			}
-			return false; // return false because we don't want tmodloader to shoot projectile
-		}*/
-
-        // What if I wanted an inaccurate gun? (Chain Gun)
-        // Inaccurate Gun style: Single Projectile, Random spread 
-        /*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(30));
-			speedX = perturbedSpeed.X;
-			speedY = perturbedSpeed.Y;
-			return true;
-		}*/
-
-        // What if I wanted multiple projectiles in a even spread? (Vampire Knives) 
-        // Even Arc style: Multiple Projectile, Even Spread 
-
-        // Help, my gun isn't being held at the handle! Adjust these 2 numbers until it looks right.
-        // public override Vector2? HoldoutOffset()
-        // {
-        // 	return new Vector2(1, 1);
-        // }
-
-        // How can I make the shots appear out of the muzzle exactly?
-        // Also, when I do this, how do I prevent shooting through tiles?
-        //public override bool shoot(player player, ref vector2 position, ref float speedx, ref float speedy, ref int type, ref int damage, ref float knockback)
-        //{
-        //	vector2 muzzleoffset = vector2.normalize(new vector2(speedx, speedy)) * 25f;
-        //	if (collision.canhit(position, 0, 0, position + muzzleoffset, 0, 0))
-        //	{
-        //		position += muzzleoffset;
-        //	}
-        //	return true;
-        //}
-
-        // How can I get a "Clockwork Assault Rifle" effect?
-        // 3 round burst, only consume 1 ammo for burst. Delay between bursts, use reuseDelay
-        /*	The following changes to SetDefaults()
-		 	item.useAnimation = 12;
-			item.useTime = 4;
-			item.reuseDelay = 14;
-		public override bool ConsumeAmmo(Player player)
-		{
-			// Because of how the game works, player.itemAnimation will be 11, 7, and finally 3. (UseAmination - 1, then - useTime until less than 0.) 
-			// We can get the Clockwork Assault Riffle Effect by not consuming ammo when itemAnimation is lower than the first shot.
-			return !(player.itemAnimation < item.useAnimation - 2);
-		}*/
-
-        // How can I shoot 2 different projectiles at the same time?
-        /*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			// Here we manually spawn the 2nd projectile, manually specifying the projectile type that we wish to shoot.
-			Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ProjectileID.GrenadeI, damage, knockBack, player.whoAmI);
-			// By returning true, the vanilla behavior will take place, which will shoot the 1st projectile, the one determined by the ammo.
-			return true;
-		}*/
-
-        // How can I choose between several projectiles randomly?
-        /*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			// Here we randomly set type to either the original (as defined by the ammo), a vanilla projectile, or a mod projectile.
-			type = Main.rand.Next(new int[] { type, ProjectileID.GoldenBullet, ProjectileType<Projectiles.ExampleBullet>() });
-			return true;
-		}*/
     }
 }
