88967a89b05abb5923db41bff15c40e3a772d78e
nosnowowie93347 <nosnowowie@Gmail.com>
2022-03-09 16:33:11 -0800

    Finally, an update!!

---
+++ NPCs/Dragon.cs
@@ -0,0 +1,54 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MyFirstBasicMod.NPCs
+{
+    // Party Zombie is a pretty basic clone of a vanilla NPC. To learn how to further adapt vanilla NPC behaviors, see https://github.com/tModLoader/tModLoader/wiki/Advanced-Vanilla-Code-Adaption#example-npc-npc-clone-with-modified-projectile-hoplite
+    public class Dragon : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dragon");
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Harpy];
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 68;
+            npc.height = 30;
+            npc.damage = 49;
+            npc.defense = 18;
+            npc.lifeMax = 200;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath2;
+            npc.value = 60f;
+            npc.knockBackResist = 0.5f;
+            npc.aiStyle = 3;
+            aiType = NPCID.Harpy;
+            animationType = NPCID.Harpy;
+            banner = Item.NPCtoBanner(NPCID.Harpy);
+            bannerItem = Item.BannerToItem(banner);
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            if (Main.hardMode) {
+                return SpawnCondition.SpiderCave.Chance * 0.1f;
+            }
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int dustType = Main.rand.Next(139, 143);
+                int dustIndex = Dust.NewDust(npc.position, npc.width, npc.height, dustType);
+                Dust dust = Main.dust[dustIndex];
+                dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.01f;
+                dust.velocity.Y = dust.velocity.Y + Main.rand.Next(-50, 51) * 0.01f;
+                dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
+            }
+        }
+    }
+}
